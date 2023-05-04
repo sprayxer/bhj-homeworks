2fa82acf25caf9414f509e325bf20063abafa4fe
sprayxer <89803128394@mail.ru>
2023-05-05 00:30:52 +0300

    edit element-search/slider

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,38 +1,39 @@
-const push = document.querySelectorAll('.slider__arrow');
-const sliderImages = [...document.getElementsByClassName('slider__image')];
-const dot = [...document.getElementsByClassName('slider__dot')];
+const sliderItems = [...document.querySelectorAll('.slider__item')]
+const sliderDots = [...document.querySelectorAll('.slider__dot')]
 
-function changeImg() {
-    const index = sliderImages.findIndex(element => element.closest('.slider__item_active'))
-    if (index < push.length && index !== -1) {
-        sliderImages[index].closest('.slider__item').classList.remove('slider__item_active')
-        sliderImages[index + 1].closest('.slider__item').classList.add('slider__item_active')
-    } else {
-        sliderImages[index].closest('.slider__item').classList.remove('slider__item_active')
-        sliderImages[0].closest('.slider__item').classList.add('slider__item_active')
+function changeSlider(e) {
+
+    const indexActive = sliderItems.findIndex(element => element === document.querySelector('.slider__item_active'))
+    sliderItems[indexActive].classList.remove('slider__item_active')
+    sliderDots[indexActive].classList.remove('slider__dot_active')
+
+    let indexSlider
+    if (e.target.classList.contains('slider__arrow_next')) {
+        indexSlider = indexActive + 1
+    } else if (e.target.classList.contains('slider__arrow_prev')) {
+        indexSlider = indexActive - 1
+    } else if (e.target.classList.contains('slider__dot')) {
+        indexSlider = sliderDots.indexOf(e.target)
     }
+
+    if (indexSlider === -1) {
+        indexSlider = sliderItems.length - 1
+    } else if (indexSlider === sliderItems.length) {
+        indexSlider = 0
+    }
+
+    sliderItems[indexSlider].classList.add('slider__item_active')
+    sliderDots[indexSlider].classList.add('slider__dot_active')
 }
 
+const sliderArrows = [...document.querySelectorAll('.slider__arrow')]
+sliderArrows.forEach(element => {
+    element.addEventListener('click', changeSlider)
+})
 
-push.forEach(item => item.onclick = changeImg)
-
-dot.forEach(item => item.onclick = function () {
-    const index = dot.findIndex(element => element.closest('.slider__dot_active'));
-    if (index != -1) {
-        dot[index].classList.remove('slider__dot_active');
-    }
-    item.classList.add('slider__dot_active');
-    changeImg();
-});
-
-
-
-
-
-
-
-
-
+sliderDots.forEach(element => {
+    element.addEventListener('click', changeSlider)
+})
 
 
 
