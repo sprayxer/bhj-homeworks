7d20ee0bcfd89f9af151eab52c48b02dad0d3841
sprayxer <89803128394@mail.ru>
2023-05-12 22:51:23 +0300

    edit dom/ads

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -2,7 +2,7 @@ const rotatorCases = [...document.querySelectorAll('.rotator__case')]
 
 let index = 0
 
-setInterval(() => {
+/*setInterval(() => {
     rotatorCases.map(element => element.classList.remove('rotator__case_active'))
     rotatorCases[index].classList.add('rotator__case_active')
     const colorText = rotatorCases[index].getAttribute('data-color')
@@ -11,5 +11,19 @@ setInterval(() => {
     if (index >= rotatorCases.length) {
         index = 0
     }
-}, 1000)
+}, 1000)*/
 
+setTimeout(function changeSpeed() {
+    rotatorCases.map(element => element.classList.remove('rotator__case_active'))
+    rotatorCases[index].classList.add('rotator__case_active')
+    const colorText = rotatorCases[index].getAttribute('data-color')
+    rotatorCases[index].style.color = colorText
+
+    const speed = rotatorCases[index].getAttribute('data-speed')
+    setTimeout(changeSpeed, speed)
+
+    index++
+    if (index >= rotatorCases.length) {
+        index = 0
+    }
+}, 0)
